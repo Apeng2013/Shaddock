7fefd4dc1f684be60cd53110f52201f85341af74
Lambert-pl <68654224+Lambert-pl@users.noreply.github.com>
2022-02-27 15:27:04 +0800

    Add event to ImGui Layer.

--- Shaddock/src/Shaddock/ImGui/ImGuiLayer.h
+++ Shaddock/src/Shaddock/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Shaddock/Layer.h"
+#include "Shaddock/Events/KeyEvent.h"
+#include "Shaddock/Events/ApplicationEvent.h"
+#include "Shaddock/Events/MouseEvent.h"
 
 namespace Shaddock {
 
@@ -14,6 +17,16 @@ namespace Shaddock {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
