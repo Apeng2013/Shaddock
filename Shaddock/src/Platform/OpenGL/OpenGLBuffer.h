6da51832cb9e9171fdb1a947efa34a2b135bdbdd
Lambert-pl <68654224+Lambert-pl@users.noreply.github.com>
2022-03-19 16:53:56 +0800

    Add vertexbuffer layout.

--- Shaddock/src/Platform/OpenGL/OpenGLBuffer.h
+++ Shaddock/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,8 +10,13 @@ namespace Shaddock {
 		virtual ~OpenGLVertexBuffer();
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_BufferLayout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_BufferLayout = layout; };
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_BufferLayout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
