5c123608381defdd828d94eca726fa2f3233fde2
Lambert-pl <68654224+Lambert-pl@users.noreply.github.com>
2022-03-19 16:12:29 +0800

    Add vertexbuffer and indexbuffer class.

--- Shaddock/src/Shaddock/Application.h
+++ Shaddock/src/Shaddock/Application.h
@@ -9,6 +9,7 @@
 #include "Shaddock/ImGui/ImGuiLayer.h"
 
 #include "Shaddock/Renderer/Shader.h"
+#include "Shaddock/Renderer/Buffer.h"
 
 namespace Shaddock {
 
@@ -33,8 +34,10 @@ namespace Shaddock {
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 	private:
 		static Application* s_Instance;
---
+++ Shaddock/src/Shaddock/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "sdpch.h"
+
+#include "Renderer.h"
+
+namespace Shaddock {
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
