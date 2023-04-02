482b904b55c22e6b95c512a8683a6734f97bd8ce
Lambert-pl <68654224+Apeng2013@users.noreply.github.com>
2023-04-02 22:18:48 +0800

    添加profile

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -17,5 +17,13 @@ private:
 	Shaddock::OrthographicCameraController m_CameraController;
 	Shaddock::Ref<Shaddock::Texture2D> m_Texture;
 
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
