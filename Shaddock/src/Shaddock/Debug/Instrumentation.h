56611aa155d6685bf8346448c6340be7c6b7fed6
Lambert-pl <68654224+Apeng2013@users.noreply.github.com>
2023-04-05 21:36:58 +0800

    Instrumentor 加锁 并增加Session检查

--- Shaddock/src/Shaddock/Debug/Instrumentation.h
+++ Shaddock/src/Shaddock/Debug/Instrumentation.h
@@ -12,7 +12,7 @@ namespace Shaddock
 	{
 		std::string Name;
 		long long Start, End;
-		uint32_t ThreadID;
+		std::thread::id ThreadID;
 	};
 
 	struct InstrumentationSession
@@ -23,50 +23,77 @@ namespace Shaddock
 	class Instrumentor
 	{
 	private:
+		std::mutex m_Mutex;
 		InstrumentationSession* m_CurrentSession;
 		std::ofstream m_OutputStream;
-		int m_ProfileCount;
 	public:
 		Instrumentor()
-			:m_CurrentSession(nullptr), m_ProfileCount(0)
+			:m_CurrentSession(nullptr)
 		{
 
 		}
 		void BeginSession(const std::string& name, const std::string& filepath = "result.json")
 		{
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession)
+			{
+				if (Log::GetCoreLogger())
+				{
+					SD_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+				}
+			}
 			m_OutputStream.open(filepath);
-			WriteHeader();
-			m_CurrentSession = new InstrumentationSession{ name };
+
+			if (m_OutputStream.is_open())
+			{
+				m_CurrentSession = new InstrumentationSession{ name };
+				WriteHeader();
+			}
+			else
+			{
+				if (Log::GetCoreLogger())
+				{
+					SD_CORE_ERROR("Instrumentor could not open results file '{0}'", filepath);
+				}
+			}
 		}
 		void EndSession()
 		{
-			WriteFooter();
-			m_OutputStream.close();
-			delete m_CurrentSession;
-			m_CurrentSession = nullptr;
-			m_ProfileCount = 0;
+			std::lock_guard lock(m_Mutex);
+			InternalEndSession();
 		}
 		void WriteProfile(const ProfileResult& result)
 		{
-			if (m_ProfileCount++ > 0)
-				m_OutputStream << ",";
-
+			std::stringstream json;
 			std::string name = result.Name;
 			std::replace(name.begin(), name.end(), '"', '\'');
-			m_OutputStream << "{";
-			m_OutputStream << "\"cat\":\"function\",";
-			m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-			m_OutputStream << "\"name\":\"" << name << "\",";
-			m_OutputStream << "\"ph\":\"X\",";
-			m_OutputStream << "\"pid\":0,";
-			m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-			m_OutputStream << "\"ts\":" << result.Start;
-			m_OutputStream << "}";
-			m_OutputStream.flush();
+			json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.End - result.Start) << ',';
+			json << "\"name\":\"" << name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.ThreadID << ",";
+			json << "\"ts\":" << result.Start;
+			json << "}";
+
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession)
+			{
+				m_OutputStream << json.str();
+				m_OutputStream.flush();
+			}
 		}
+		static Instrumentor& Get()
+		{
+			static Instrumentor instance;
+			return instance;
+		}
+
+	private:
 		void WriteHeader()
 		{
-			m_OutputStream << "{\"otherData\": {}, \"traceEvents\":[";
+			m_OutputStream << "{\"otherData\": {}, \"traceEvents\":[{}";
 			m_OutputStream.flush();
 		}
 
@@ -75,11 +102,15 @@ namespace Shaddock
 			m_OutputStream << "]}";
 			m_OutputStream.flush();
 		}
-
-		static Instrumentor& Get()
+		void InternalEndSession()
 		{
-			static Instrumentor instance;
-			return instance;
+			if (m_CurrentSession)
+			{
+				WriteFooter();
+				m_OutputStream.close();
+				delete m_CurrentSession;
+				m_CurrentSession = nullptr;
+			}
 		}
 	};
 
@@ -101,8 +132,7 @@ namespace Shaddock
 			auto endTimepoint = std::chrono::high_resolution_clock::now();
 			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
 			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-			Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+			Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id() });
 			m_Stopped = true;
 		}
 
